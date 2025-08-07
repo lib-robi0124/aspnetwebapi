744e14dcc0321a44e9f86f1b7d107f204de257a7
lib-robi0124 <robert.ristovski@libertysteelgroup.com>
2025-08-07 20:38:25 +0200

    cl2

---
+++ class02/class02/class02/Controllers/NotesController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace class02.Controllers
+{
+    [Route("api/[controller]")] //http://localhost:[port]/api/notes 
+    [ApiController]
+    public class NotesController : ControllerBase
+    {
+        [HttpGet] //http://localhost:[port]/api/notes 
+        public ActionResult<List<string>> Get()
+        {
+            //return StatusCode(StatusCodes.Status200OK, StaticDb.SimpleNotes);
+            return Ok(StaticDb.SimpleNotes);
+        }
+        [HttpGet("{index}")]
+        public ActionResult<string> GetByIndex(int index)
+        {
+            try
+            {
+                if (index < 0)
+                {
+                    return StatusCode(StatusCodes.Status400BadRequest, "The index has negative value");
+                }
+
+                if (index <= StaticDb.SimpleNotes.Count)
+                {
+                    return StatusCode(StatusCodes.Status404NotFound, $"There is no resourse on index {index}");
+                }
+                return StatusCode(StatusCodes.Status200OK, StaticDb.SimpleNotes[index]);
+            }
+            catch (Exception e) //for some thing what we can not catch 
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError, "an error occurred.Contact admin");
+            }
+        }
+        [HttpPost]
+        public ActionResult Post([FromBody] string newNote)
+        {
+            try
+            {
+                using (StreamReader reader = new StreamReader(Request.Body))
+                {
+                    //string newNote = reader.ReadToEnd();
+
+                    if (string.IsNullOrEmpty(newNote))
+                    {
+                        return BadRequest("the body can not be empty!");
+                    }
+                    StaticDb.SimpleNotes.Add(newNote);
+                    return StatusCode(StatusCodes.Status201Created, "the new note added");
+                }
+            }
+            catch (Exception e)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError, "an error occurred.Contact admin");
+            }
+        }
+    }
+}
---
+++ class02/class02/class02/StaticDb.cs
@@ -0,0 +1,12 @@
+﻿namespace class02
+{
+    public static class StaticDb
+    {
+        public static List<string> SimpleNotes = new List<string>
+        {
+            "Do the homework",
+            "Go to the Gym",
+            "Buy breakfast"
+        };
+    }
+}
